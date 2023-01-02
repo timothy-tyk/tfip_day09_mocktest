e739fde62dc1d144f2efd5e9caa842cae8b7110f
timothy-tyk <104057692timothy-tyk@users.noreply.github.com>
2023-01-02 16:13:33 +0800

    test commit

--- redo/src/myserver/Main.java
+++ redo/src/myserver/Main.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class Main {
   public static void main(String[] args) {
-    Integer PORT=3000;
+    Integer PORT=3001;
     String[] directories = {"./target"};
 
     for(Integer i=0;i<args.length;i++){
